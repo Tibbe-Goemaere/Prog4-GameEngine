e61dee10a3b323fa23116a91238f182d46e9c00b
TibbeG <tibbegoemaere@icloud.com>
2023-03-19 17:50:15 +0100

    Startup ImGui

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -1,20 +1,17 @@
 #include <SDL.h>
 #include "InputManager.h"
+#include "imgui_impl_sdl2.h"
 
 bool dae::InputManager::ProcessInput()
 {
+	
 	SDL_Event e;
 	while (SDL_PollEvent(&e)) {
 		if (e.type == SDL_QUIT) {
 			return false;
 		}
-		if (e.type == SDL_KEYDOWN) {
-			
-		}
-		if (e.type == SDL_MOUSEBUTTONDOWN) {
-			
-		}
-		// etc...
+		
+		ImGui_ImplSDL2_ProcessEvent(&e);
 	}
 
 	return true;
